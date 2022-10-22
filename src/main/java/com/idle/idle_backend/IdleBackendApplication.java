c50826ee42f3724b81ba851b53462369e6ed27e2
wellbeing-dough <kdw990202@naver.com>
2022-10-22 17:58:41 +0900

    프로젝트 생성

---
+++ src/main/java/com/idle/idle_backend/IdleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.idle.idle_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdleBackendApplication.class, args);
+	}
+
+}
