d41b815477b00105742779a241f918dafca2fad0
wellbeing-dough <kdw990202@naver.com>
2023-01-17 03:02:11 +0900

    JwtAuthenticationFilter 생성

---
+++ src/main/java/com/idle/idle_backend/config/jwt/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,52 @@
+package com.idle.idle_backend.config.jwt.filter;
+
+import com.idle.idle_backend.config.jwt.JwtTokenProvider;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+
+@Getter
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Builder
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String baseUrl = "http://localhost:8080/api";
+        StringBuffer requestURL = request.getRequestURL();
+        if(requestURL.toString().equals(baseUrl + "/oauth/login") || requestURL.toString().equals(baseUrl + "/user/addInfo")
+                || requestURL.toString().equals(baseUrl + "/vote/createVote") || requestURL.toString().equals(baseUrl + "/vote/updateVote")
+                || requestURL.toString().contains(baseUrl + "/vote/deleteVote/")) {
+            String authorizationHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+            try {
+                HashMap<String, Object> parseJwtTokenMap = jwtTokenProvider.parseJwtToken(authorizationHeader);
+                Claims claims = (Claims)parseJwtTokenMap.get("claims");
+                String token = (String) parseJwtTokenMap.get("token");
+                request.setAttribute("claims", claims); // jwt 정보 컨트롤러에서 사용할 수 있게 request에 담기
+                request.setAttribute("token",token);
+            } catch (ExpiredJwtException jwtException) {
+                throw jwtException;
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+}
