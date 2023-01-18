7df13be506482199c02bc1b5d658ae8bd4340a3e
wellbeing-dough <kdw990202@naver.com>
2023-01-18 23:29:43 +0900

    NaverController 구현

---
+++ src/main/java/com/idle/idle_backend/config/oauth2/social/controller/NaverController.java
@@ -0,0 +1,32 @@
+package com.idle.idle_backend.config.oauth2.social.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.json.simple.parser.ParseException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/oauth")
+public class NaverController {
+
+    private final NaverService naverService;
+
+
+    @PostMapping("/naver")
+    public ResponseEntity<GetLoginTokenResponse> getNaverToken(@Valid @RequestBody GetNaverToken getnaverToken) throws IOException, ParseException {
+        String code = getnaverToken.getCode();
+        String state = getnaverToken.getState();
+        GetLoginTokenResponse getLoginTokenResponse = naverService.NaverLogin(code, state);
+        return new ResponseEntity(getLoginTokenResponse, HttpStatus.OK);
+    }
+
+}
