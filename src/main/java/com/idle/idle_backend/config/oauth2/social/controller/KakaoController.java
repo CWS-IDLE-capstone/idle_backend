efc3ec1e8da98b6d6a92d66b694f5358051faaef
wellbeing-dough <kdw990202@naver.com>
2023-01-18 23:30:52 +0900

    KakaoController 구현

---
+++ src/main/java/com/idle/idle_backend/config/oauth2/social/controller/KakaoController.java
@@ -0,0 +1,62 @@
+package com.idle.idle_backend.config.oauth2.social.controller;
+
+import io.jsonwebtoken.Claims;
+import lombok.RequiredArgsConstructor;
+import org.json.simple.parser.ParseException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.util.Optional;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/oauth")
+public class KakaoController {
+
+    private final KakaoService kakaoService;
+    private final UserRepository userRepository;
+
+    /**
+     * 카카오 서버에서 유저정보 조희
+     *
+     * @param getkakaoToken
+     * @return 엑세스 토큰
+     * @throws IOException
+     * @throws ParseException
+     */
+    @PostMapping("/kakao")
+    public ResponseEntity<GetLoginTokenResponse> getKaKaoToken(@Valid @RequestBody GetkakaoTokenRequest getkakaoToken) throws IOException, ParseException {
+        String code = getkakaoToken.getCode();
+        String redirectUrl = getkakaoToken.getRedirectUrl();
+        GetLoginTokenResponse getLoginToken = kakaoService.KakaoLogin(code, redirectUrl);
+        return new ResponseEntity(getLoginToken, HttpStatus.OK);
+    }
+
+
+    @GetMapping("/login")
+    public ResponseEntity<GetUserInfoResponse> getUserInfo(@RequestAttribute Claims claims) {
+        //엑세스 토큰안의 유저 아이디로 유저를 찾은 다음 유저정보 리턴해줌
+        Integer userId = (int) claims.get("userId");
+        Long longId = Long.valueOf(userId);
+        //userId로 유저 꺼내기
+        Optional<User> result = userRepository.findById(longId);
+        User user = result.get();
+
+        GetUserInfoResponse getUserInfo = GetUserInfoResponse.builder()
+                .username(user.getNickname())
+                .email(user.getEmail())
+                .imageUrl(user.getImageUrl())
+                .provider(user.getProvider())
+                .userId(user.getId())
+                .age(user.getAge())
+                .gender(user.getGender())
+                .mbti(user.getMbti())
+                .message("유저 정보 요청에 성공했습니다.")
+                .build();
+
+        return new ResponseEntity(getUserInfo, HttpStatus.OK);
+    }
+}
