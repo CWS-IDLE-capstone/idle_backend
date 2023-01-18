a424c73f1a93db833ef636b73f5ae898afc33adf
wellbeing-dough <kdw990202@naver.com>
2023-01-19 02:53:42 +0900

    카카오로그인 구현

--- src/main/java/com/idle/idle_backend/user/domain/UserRepository.java
+++ src/main/java/com/idle/idle_backend/user/domain/UserRepository.java
@@ -8,5 +8,8 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findById(Long Id);
+
+    Optional<User> findByProviderId(String providerId);
+
     Boolean existsByProviderId(String providerId);
 }
