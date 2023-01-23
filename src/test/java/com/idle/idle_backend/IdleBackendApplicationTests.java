583b45a02cbf05e30fb0e143fe0677936d4391b1
wellbeing-dough <kdw990202@naver.com>
2023-01-24 01:14:44 +0900

    테스트 코드는 dev 환경에서만 돌아가게 변경

--- src/test/java/com/idle/idle_backend/IdleBackendApplicationTests.java
+++ src/test/java/com/idle/idle_backend/IdleBackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.idle.idle_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-//@SpringBootTest
+@ActiveProfiles("dev")
+@SpringBootTest
 class IdleBackendApplicationTests {
 
 	@Test
