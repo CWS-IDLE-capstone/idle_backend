f74d3a9702b76dc4c5c31392344b86322a0e627b
alsduq1117 <alsduq1117@naver.com>
2023-02-13 23:11:28 +0900

    Style : push 알림 Controller 이름 MainController -> PushController

--- src/main/java/com/idle/idle_backend/push/controller/PushController.java
+++ src/main/java/com/idle/idle_backend/push/controller/PushController.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor
-public class MainController {
+public class PushController {
 
     private final FirebaseCloudMessageService firebaseCloudMessageService;
 
