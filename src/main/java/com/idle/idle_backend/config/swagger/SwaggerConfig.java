4c357ca77d785bab95646c8756bcf69869ede4a0
wellbeing-dough <kdw990202@naver.com>
2023-02-15 15:27:09 +0900

    feat : 스웨거 배포 환경 적용

--- src/main/java/com/idle/idle_backend/config/swagger/SwaggerConfig.java
+++ src/main/java/com/idle/idle_backend/config/swagger/SwaggerConfig.java
@@ -1,27 +1,53 @@
 package com.idle.idle_backend.config.swagger;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(title = "API 명세서",
-                description = "최원석과 IDLE API 명세서 입니다.",
-                version = "v1"))
-
+@OpenAPIDefinition(servers = { @Server(url = "http://awsv4-env.eba-mre2mcnv.ap-northeast-2.elasticbeanstalk.com/"),
+        @Server(url = "http://localhost:8080/")})
 @Configuration
-@RequiredArgsConstructor
 public class SwaggerConfig {
 
     @Bean
-    public GroupedOpenApi publicAPi(){
-
+    public GroupedOpenApi publicApi() {
         return GroupedOpenApi.builder()
                 .group("v1-definition")
                 .pathsToMatch("/api/**")
                 .build();
     }
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+
+        // SecuritySecheme명
+        String jwtSchemeName = "jwtAuth";
+
+        // API 요청헤더에 인증정보 포함
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        // SecuritySchemes 등록
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP) // HTTP 방식
+                        .scheme("bearer")
+                        .bearerFormat("JWT")); // 토큰 형식을 지정하는 임의의 문자(Optional)
+
+
+        return new OpenAPI()
+                .info(new Info().title("IDLE Project API")
+                        .description("프로젝트 API 명세서입니다.")
+                        .version("v0.0.1"))
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
 }
