5c050d557261884457edffd90985ba3461eea5e8
wellbeing-dough <kdw990202@naver.com>
2023-02-26 01:42:44 +0900

    refact : GetNaverToken 스웨거에 설명 추가

--- src/main/java/com/idle/idle_backend/config/oauth2/social/dto/GetNaverToken.java
+++ src/main/java/com/idle/idle_backend/config/oauth2/social/dto/GetNaverToken.java
@@ -1,14 +1,20 @@
 package com.idle.idle_backend.config.oauth2.social.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
 
 @Getter
 public class GetNaverToken {
+
+    @Schema(description = "네이버 서버로부터 받아온 인가코드",
+            example = "인가코드")
     @NotBlank
     String code;
 
+    @Schema(description = "상태값 일단 test",
+            example = "test")
     @NotBlank
     String state;
 
